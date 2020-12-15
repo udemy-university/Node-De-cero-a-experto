928800a6596c3816c8799db18013fef8f6728fc3
Sandro Dezerio <sdezerio@gmail.com>
2020-12-14 23:02:01 -0300

    fin sección 12

--- 07 - REST Server/server/routes/producto.js
+++ 07 - REST Server/server/routes/producto.js
@@ -8,7 +8,7 @@ const Producto = require('../models/producto');
 //	================
 //	Obtener productos
 //	================
-app.get('/productos', (req, res) => {
+app.get('/productos', verificaToken, (req, res) => {
     // traer todos los productos
     // populate: usuario, categoria
     // paginado
@@ -19,7 +19,7 @@ app.get('/productos', (req, res) => {
     let limite = req.query.limite || 5;
     limite = Number(limite);
 
-    Producto.find({})
+    Producto.find({ disponible: true})
         .skip(desde)
         .limit(limite)
         .sort('descripcion')
@@ -48,16 +48,17 @@ app.get('/productos', (req, res) => {
 //	================
 //	Obtener producto por id
 //	================
-app.get('/productos/:id', (req, res) => {
+app.get('/productos/:id', verificaToken, (req, res) => {
     // traer todos los productos
     // populate: usuario, categoria
 
     let id = req.params.id;
 
     Prodcuto
+        .findById(id)
         .populate('usuario', 'nombre email')
-        .populate('categoria', 'descripcion')
-        .findById( id, (err, productoDB) => {
+        .populate('categoria', 'nombre')
+        .exec((err, productoDB) => {
 
         if(err) {
             return res.status(500).json({
@@ -83,9 +84,37 @@ app.get('/productos/:id', (req, res) => {
 });
 
 //	================
+//	Buscar productos
+//	================
+app.get('/productos/buscar/:termino', verificaToken, (req, res) => {
+
+    let termino = req.params.termino;
+
+    let regex = new RegExp(termino, 'i'); //insensible a mayusculas y minusculas
+
+    Producto
+        .find({ nombre: regex })
+        .populate('categoria', 'nombre')
+        .exec((err, productos) => {
+
+            if(err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            res.json({
+                ok: true,
+                productos
+            })
+        })
+});
+
+//	================
 //	Crear un nuevo producto
 //	================
-app.post('/productos', (req, res) => {
+app.post('/productos', verificaToken, (req, res) => {
     // grabar el usuario
     // grabar una categoria del listado
 
@@ -119,7 +148,7 @@ app.post('/productos', (req, res) => {
 //	================
 //	Modificar un producto
 //	================
-app.put('/productos/:id', (req, res) => {
+app.put('/productos/:id', verificaToken, (req, res) => {
     // traer todos los productos
     // populate: usuario, categoria
 
@@ -139,7 +168,7 @@ app.put('/productos/:id', (req, res) => {
             return res.status(400).json({
                 ok: false,
                 err: {
-                    message: 'El Id no existe';
+                    message: 'El Id no existe'
                 }
             });
         }
@@ -170,8 +199,46 @@ app.put('/productos/:id', (req, res) => {
 //	================
 //	Obtener producto por id
 //	================
-app.delete('/productos/:id', (req, res) => {
+app.delete('/productos/:id', verificaToken, (req, res) => {
 
+    let id = req.params.id;
+
+    Producto.findById( id, (err, productoDB) => {
+
+        if(err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if(!productoDB) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'El Id no existe'
+                }
+            });
+        }
+
+        productoDB.disponible = false;
+
+        productoDB.save((err, productoBorrado) => {
+
+            if(err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            res.json({
+                ok: true,
+                producto: productoBorrado,
+                mensaje: 'Producto borrado'
+            })
+        })
+    })
 });
 
 module.exports = app;
