9536721b42ee9f878b5806a4d17c59110d2e92a4
Sandro Dezerio <sdezerio@gmail.com>
2020-12-16 22:27:31 -0300

    fin de la sección 13

--- 07 - REST Server/server/middlewares/autenticacion.js
+++ 07 - REST Server/server/middlewares/autenticacion.js
@@ -45,7 +45,33 @@ let verificaAdminRole = (req, res, next) => {
     }
 }
 
+//	================
+//	Verificar Token IMG
+//	================
+
+let verificaTokenImg = (req, res, next) => {
+    
+    const token = req.query.token;
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+
+        if(err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token no válido.'
+                }
+            })
+        }
+
+        req.usuario = decoded.usuario;
+
+        next();
+    });
+}
+
 module.exports = {
     verificaToken,
-    verificaAdminRole
+    verificaAdminRole,
+    verificaTokenImg
 }
