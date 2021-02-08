052de3457b8a866193b1b8758508daabf3f73bcd
Sandro Dezerio <sdezerio@gmail.com>
2021-02-08 11:32:31 -0300

    finalizamos el chat

--- 10 - Socket Chat/public/js/socket-chat.js
+++ 10 - Socket Chat/public/js/socket-chat.js
@@ -2,22 +2,25 @@ var socket = io();
 
 var params = new URLSearchParams(window.location.search);
 
-if(!params.has('nombre') || !params.has('sala')) {
+if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
-    throw new Error('El nombre y la sala son necesarios');
+    throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-}
+};
+
+
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
-    socket.emit('entrarChat', usuario, function(res) {
-        console.log(res);
+    socket.emit('entrarChat', usuario, function(resp) {
+        renderizarUsuarios(resp);
     });
+
 });
 
 // escuchar
@@ -29,26 +32,29 @@ socket.on('disconnect', function() {
 
 
 // Enviar información
-socket.emit('crearMensaje', {
-    mensaje: 'Hola Mundo'
-}, function(resp) {
-    console.log('respuesta server: ', resp);
-});
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    // console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
-//Escuchar cambios de usuarios, cuando un usuario entra o sale del chat.
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
+socket.on('listaPersona', function(personas) {
+    renderizarUsuarios(personas);
+});
 
-socket.on('listaPersona', function(personas){
-    console.log(personas);
-})
-
-//Mensajes privados
+// Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
-    console.log("msg priv", mensaje);
+
+    console.log('Mensaje Privado:', mensaje);
+
 });
